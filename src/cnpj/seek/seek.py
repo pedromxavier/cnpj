91e767c5abdd477fa11e19ac7dc2e7c775a88022
Pedro <pedromxavier@poli.ufrj.br>
2021-03-14 00:33:52 -0300

    <3

--- src/cnpj/seek/seek.py
+++ src/cnpj/seek/seek.py
@@ -97,7 +97,7 @@ def retrieve(ifile, indices: list):
                 file.seek(int(seek))
                 block = file.read(1200)
 
-            found[cnpj] = block
+            found[cnpj] = str(block)
 
     return {
         'found': found,
