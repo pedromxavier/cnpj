4ff4f68873e86abcb78d31a56c9f90a84e8e3c55
Pedro <pedromxavier@poli.ufrj.br>
2021-03-14 00:18:00 -0300

    <3

--- src/cnpj/index/index.py
+++ src/cnpj/index/index.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 import sys
 import os
 import re
@@ -22,78 +23,89 @@ T_PERSON = sys.intern('2')
 T_CNAESC = sys.intern('6')
 T_TRAILL = sys.intern('9')
 
-ENDL = '\n'.encode('utf-8')
-BLOCK_SIZE = 1200
+ENDL = sys.intern('\n')
+
 FILE_INDEX = tuple(range(1, 21))
 
-def read_block(file) -> (str, bytes):
-    block = file.read(1200).decode(encoding='utf8')
-    s = file.read(1) ## sep
-    return (block, s)
-
-def read_entry(file, ifile, seek:int, i: int) -> (dict, bool):
+def read_entry(file, ifile, i: int) -> (dict, bool):
     global T_HEADER, T_ENTERP, T_PERSON, T_CNAESC, T_TRAILL, ENDL
-
-    block, s = read_block(file)
-
     # Entry Type
-    c = sys.intern(block[0])
+    c = sys.intern(file.read(1).decode('utf-8'))
 
     if c is T_HEADER:
-        return None if not s else False
+        code = read_header(file)
     elif c is T_ENTERP:
-        return read_enterp(block, ifile, seek=seek, i=i)
+        code = read_enterp(file, ifile, i)
     elif c is T_PERSON:
-        return None if not s else False
+        code = read_header(file)
     elif c is T_CNAESC:
-        return None if not s else False
+        code = read_header(file)
     elif c is T_TRAILL:
-        return None if not s else False
+        code = read_header(file)
     elif not c:
         return None
     else:
         raise ReadError(f"Invalid char <{c} @ {file.tell()}>")
 
-def read_header(block: str):
+    c = sys.intern(file.read(1).decode('utf-8'))
+
+    if not c: # trailing endl or EOF
+        return None
+    elif c is not ENDL:
+        raise ReadError(f"Invalid endl <{c}> @ {file.tell()}")
+    else:
+        return code
+
+def read_header(file):
     # Discard content
+    file.seek(1199, os.SEEK_CUR)
     return False
 
-def read_enterp(block, ifile, seek: int, i: int=0) -> bool:
+def read_enterp(file, ifile, i: int=0) -> bool:
     # Get initial position
-    ifile.write(f"{block[3:17]}{i:02d}{seek:024d}".encode('utf-8'))
+    seek = file.tell()
+    
+    # Discard some things
+    file.seek(2, os.SEEK_CUR)
+
+    cnpj = file.read(14).decode('utf-8')
+
+    ifile.write(f"{cnpj}{i:02d}{seek:024d}".encode('utf-8'))
+
+    # Discard some things
+    file.seek(1183, os.SEEK_CUR)
+
     return True
+
+
     
 def index(args: argparse.Namespace):
     global FILE_INDEX, PATH
 
-    size = 0
     code = None
+    size = 0
     with open_local('cnpj.index', path=args.path, mode='wb') as ifile:
-        # Reserve space for header BLOCK = 40 bytes
         ifile.write(f"{size:040d}".encode('utf-8'))
-
         for i in FILE_INDEX:
             fname = PATH.format(i)
             print(f"Indexing <{fname}>", end='\r')
             with open_local(fname, path=args.path, mode='rb') as file:
-                # We don't know the actual size for all contents.
                 while True:
                     try:
-                        code = read_entry(file, ifile, file.tell(), i)
+                        code = read_entry(file, ifile, i)
                     except ReadError as error:
                         print(f"ReadError: {error.msg}")
                         return
-
-                    if code is None:
-                        break
-                    elif code is True:
-                        size += 1
-                    elif code is False:
-                        continue
+                    else:
+                        if code is None:
+                            break
+                        elif code is True:
+                            size += 1
+                        else:
+                            continue
         else:
             ifile.seek(0)
             ifile.write(f"{size:040d}".encode('utf-8'))
-
     print("All files indexed." + " " * 40)
 
     print("Sorting Index")
@@ -104,5 +116,4 @@ def index(args: argparse.Namespace):
     with open_local('cnpj.index', path=args.path, mode='wb') as ifile:
         ifile.write(f"{size:040d}".encode('utf-8'))
         ifile.write(data)
-
     print("Finished indexing data.")
