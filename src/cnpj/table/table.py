cb68137c3107aab74be2f2d2b47ef1b9bb773f22
Pedro <pedromxavier@poli.ufrj.br>
2021-03-15 04:17:01 -0300

    <3

--- src/cnpj/table/table.py
+++ src/cnpj/table/table.py
@@ -3,6 +3,9 @@ import csv
 import json
 import argparse
 
+def excel_str(s: object):
+    return f'="{s}"'
+
 
 def table(args: argparse.Namespace):
     with open(args.file, "r") as jfile:
@@ -13,25 +16,26 @@ def table(args: argparse.Namespace):
     with open(f"{fname}.csv", "w") as cfile:
         fields = ["cnpj", "matriz", "nome", "fantasia", "cnae", "cep", "cnaesec"]
         writer = csv.DictWriter(cfile, fields)
+        writer.writeheader()
 
         for key in data["found"]:
             entry = data["found"][key]
             writer.writerow(
                 {
-                    "cnpj": entry["cnpj"],
+                    "cnpj": excel_str(entry["cnpj"]),
                     "matriz": "S" if entry["matriz"] else "N",
-                    "nome": entry["nome"],
-                    "fantasia": entry["fantasia"],
-                    "cnae": entry["cnae"],
-                    "cep": entry["cep"],
-                    "cnaesec": ";".join(entry["cnaesec"]),
+                    "nome": excel_str(entry["nome"]),
+                    "fantasia": excel_str(entry["fantasia"]),
+                    "cnae": excel_str(entry["cnae"]),
+                    "cep": excel_str(entry["cep"]),
+                    "cnaesec": excel_str(";".join(entry["cnaesec"])),
                 }
             )
 
         for cnpj in data["missing"]:
             writer.writerow(
                 {
-                    "cnpj": cnpj,
+                    "cnpj": excel_str(cnpj),
                     "matriz": "",
                     "nome": "",
                     "fantasia": "",
